ce33d24434abaf58165f7d304b642ca7b2280baa
ledautopica <vane.multimedia@gmail.com>
2023-05-22 18:01:05 -0600

    ajustes

--- src/controllers/project.js
+++ src/controllers/project.js
@@ -1,29 +1,44 @@
-const ProjectServices = require("../services/projects");
+const ProjectService = require("../services/projects");
 
 exports.getProjects = async (req, res) => {
     try {
-        let projects = await ProjectServices.getProjects();
+        let projects = await ProjectService.getProjects();
         res.json({
             projects:projects,
         });
     }catch(err){
         console.error("err", err);
-        res.status(500),json({
+        res.status(500).json({
             message:"Projects were not retrieved",
         });
     }
 };
 
-exports.getProjectById = async (id) => {
+exports.getProjectById = async (req, res) => {
     try {
-        let project = ProjectService.getProjectById(req.params.id)
+        let project = await ProjectService.getProjectById(req.params.id)
         res.json({
-            project
+            project: project,
         })
     } catch (err){
         console.error("err",err);
-        rest.status(404).json({
+        res.status(404).json({
             message:"Project was not found",
-        })
+        });
     }
-}
+};
+
+exports.createProject = async (req,res) => {
+    try{
+        let projectSaved = await ProjectService.createProject(req.body)
+        res.status(201).json({
+            message:"Project created",
+            projectSaved: projectSaved
+        })
+    } catch (err){
+        console.err("err", err);
+        res.status(400).json({
+            message: "Was not able to create the project",
+        });
+    }
+};
