5c218f176677c59d6572fa85760e0755dda655cf
ledautopica <vane.multimedia@gmail.com>
2023-05-29 18:45:16 -0600

    code coverage, Integration and Load parte 2

--- src/services/__tests__/project-service.integration.test.js
+++ src/services/__tests__/project-service.integration.test.js
@@ -12,26 +12,60 @@ afterAll(async () => {
     await mongoose.disconnect();
 });
 
+const projectOne = {
+    name: "Project one",
+    projectLink: "projectone.com",
+    description: "This is a first project",
+    overview:"a brief overview of the project",
+    imageUrl:"projectone.com/image.png",
+    tools: ["HTML", "CSS","Java"],
+};
+const projectTwo = {
+    name: "Project two",
+    projectLink: "projecttwo.com",
+    description: "This is the second project",
+    overview:"a brief overview of the project",
+    imageUrl:"projecttwo.com/image.png",
+    tools: ["HTML", "Python"],
+};
+
 describe("GET /projects", () => {
-    const projectOne = {
-        name: "Project one",
-        projectLink: "projectone.com",
-        description: "This is a first project",
-        overview:"a brief overview of the project",
-        imageUrl:"projectone.com/image.png",
-        tools: ["HTML", "CSS","Java"],
-    };
+    
     it("should return all projects in database", async() => {
         await Project.deleteMany();
         await Project.create(projectOne);
+        await Project.create(projectTwo);
 
         const response = await request(app).get("/projects");
         expect(response.status).toBe(200);
+
         const projects = response.body.projects;
+        
         expect(Array.isArray(projects)).toBe(true);
-        expect(projects.length).toEqual(1);
+        expect(projects.length).toEqual(2);
         expect(projects).toEqual(
-            expect.arrayContaining([expect.objectContaining(projectOne)])
+            expect.arrayContaining([expect.objectContaining(projectOne)]),
+            expect.arrayContaining([expect.objectContaining(projectTwo)])
         );
     });
 });
+
+describe("POST /projects", () => {
+    it("should create a new projects and return a created status code", async () => {
+        const response = await request(app).post("/projects").send(projectOne);
+    expect(response.statusCode).toBe(201);
+    console.log(response.body.projectSaved);
+    expect(response.body.projectSaved).toEqual(
+        expect.objectContaining({
+            _id: projectOne.any(String),
+            name: projectOne.name,
+            projectLink: projectOne.projectLink,
+            description: projectOne.description,
+            overview: projectOne.overview,
+            imageUrl: projectOne.imageUrl,
+            tools: projectOne.tools,
+    })
+    );
+
+    });
+    });
