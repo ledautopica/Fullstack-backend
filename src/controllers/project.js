ff79d5adf76ef06fd02d241677ce19537e0d9cde
ledautopica <vane.multimedia@gmail.com>
2023-06-08 00:15:51 -0600

    revision controllers

--- src/controllers/project.js
+++ src/controllers/project.js
@@ -1,62 +1,58 @@
 const ProjectService = require("../services/projects");
 
 exports.getProjects = async (req, res) => {
-    try {
-        let projects = await ProjectService.getProjects();
-        console.log("got hit!");
-        res.json({
-            projects:projects,
-        });
-    }catch(err){
-        console.error("err", err);
-        res.status(500).json({
-            message:"Projects were not retrieved",
-        });
-    }
+  try {
+    const projects = await ProjectService.getProjects();
+    res.json({
+      projects: projects,
+    });
+  } catch (err) {
+    console.error("err", err);
+    res.status(500).json({
+      message: "Project were not retrieved",
+    });
+  }
 };
 
 exports.getProjectById = async (req, res) => {
-    try {
-        let project = await ProjectService.getProjectById(req.params.id)
-        console.log(project);
-        res.json({
-            project: project,
-        })
-    } catch (err){
-        console.error("err",err);
-        res.status(404).json({
-            message:"Project was not found",
-        });
-    }
+  try {
+    let project = await ProjectService.getProjectById(req.params.id);
+    res.json({
+      project: project,
+    });
+  } catch (err) {
+    console.error("err", err);
+    res.status(404).json({
+      message: "Project was not found",
+    });
+  }
 };
 
-exports.createProject = async (req,res) => {
-    try{
-        let projectSaved = await ProjectService.createProject(req.body)
-        res.status(201).json({
-            message:"Project created",
-            projectSaved: projectSaved
-        })
-    } catch (err){
-        console.err("err", err);
-        res.status(400).json({
-            message: "Was not able to create the project",
-        });
-    }
+exports.createProject = async (req, res) => {
+  try {
+    let projectSaved = await ProjectService.createProject(req.body);
+    res.status(201).json({
+      message: "Project created",
+      projectSaved: projectSaved,
+    });
+  } catch (err) {
+    console.error("err", err);
+    res.status(400).json({
+      message: "Was not able to create the project",
+    });
+  }
 };
 
 exports.updateProject = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const projectData = req.body;
-    
-        const updateProject = await ProjectService.updateProject(id, projectData)
-        
-        res.status(200).json(updateProject);
+  try {
+    const { id } = req.params;
+    const projectData = req.body;
 
-    }catch (error){
-        console.error(error);
-        res.status(500).json({message:"Internal error"});
-    }
- 
-};
+    const updatedProject = await ProjectService.updateProject(id, projectData);
+
+    res.status(200).json(updatedProject);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal error" });
+  }
+}
